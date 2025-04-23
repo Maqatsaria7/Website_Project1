61f3ef840a7e3707874f66a0cd217c85592b6397
Sandro Makatsaria <sandromakatsaria2009@gmail.com>
2025-04-23 22:26:55 +0400

    added contact_us, unfinished

--- app_main.py
+++ app_main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
+items = []
 @app.route("/")
 def main_page():
     return render_template("main_page.html")
 
+@app.route("/contact_us", methods=["GET", "POST"])
+def contact_us():
+    if request.method == 'POST':
+        name = request.form.get("name")
+        email = request.form.get("email")
+        question = request.form.get("question")
+        items.append({"name": name, "email": email, "question": question})
+    return render_template("contact_us.html")
 if "__main__" == __name__:
     app.run(debug=True)
