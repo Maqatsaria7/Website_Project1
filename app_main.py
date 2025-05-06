647423fe16e81385ea85b1c6f7107cdf4dcfa27a
Sandro Makatsaria <sandromakatsaria2009@gmail.com>
2025-05-06 19:56:18 +0400

    added make a post, workout, nutrition, supplement, gave make a post function, made new table posts

--- app_main.py
+++ app_main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
-items = []
+
 @app.route("/")
 def main_page():
     return render_template("main_page.html")
@@ -12,8 +13,64 @@ def contact_us():
         name = request.form.get("name")
         email = request.form.get("email")
         question = request.form.get("question")
-        items.append({"name": name, "email": email, "question": question})
-        print(items)
+        with sqlite3.connect('fitnessproject.db') as conn:
+            conn.execute('''
+                INSERT INTO contacts (name, email, question) VALUES (?, ?, ?)
+            ''', (name, email, question))
     return render_template("contact_us.html")
-if "__main__" == __name__:
+
+@app.route("/about_us")
+def about_us():
+    return render_template("about_us.html")
+
+@app.route("/make_a_post", methods=["GET", "POST"])
+def make_a_post():
+    if request.method == 'POST':
+        title = request.form.get("title")
+        author = request.form.get("author")
+        description = request.form.get("description")
+        information = request.form.get("information")
+        category = request.form.get("category")
+        with sqlite3.connect('fitnessproject.db') as conn:
+            conn.execute('''
+                INSERT INTO posts (title, author, description, information, category) 
+                VALUES (?, ?, ?, ?, ?)
+            ''', (title, author, description, information, category))
+    return render_template("make_a_post.html")
+
+@app.route("/workouts", methods=["GET"])
+def workouts():
+    with sqlite3.connect('fitnessproject.db') as conn:
+        c = conn.cursor()
+        c.execute('''
+            SELECT * FROM posts WHERE category = ?
+        ''', ("TRAINING",))
+        posts = c.fetchall()
+    return render_template("workouts.html", posts=posts)
+
+
+@app.route("/nutrition", methods=["GET"])
+def nutrition():
+    with sqlite3.connect('fitnessproject.db') as conn:
+        c = conn.cursor()
+        c.execute('''
+            SELECT * FROM posts WHERE category = ?
+        ''', ("NUTRITION",))
+        posts = c.fetchall()
+    return render_template("nutrition.html", posts=posts)
+
+
+@app.route("/supplement", methods=["GET"])
+def supplement():
+    with sqlite3.connect('fitnessproject.db') as conn:
+        c = conn.cursor()
+        c.execute('''
+            SELECT * FROM posts WHERE category = ?
+        ''', ("SUPPLEMENTS",))
+        posts = c.fetchall()
+    return render_template("supplement.html", posts=posts)
+
+
+
+if __name__ == "__main__":  # <-- correct comparison operator
     app.run(debug=True)
---
+++ database_creation.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+conn = sqlite3.connect('fitnessproject.db')
+c = conn.cursor()
+
+conn.execute('''
+            CREATE TABLE IF NOT EXISTS contacts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                email TEXT NOT NULL,
+                question TEXT NOT NULL
+            )
+        ''')
+conn.execute('''
+            CREATE TABLE IF NOT EXISTS posts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                author TEXT NOT NULL,
+                description TEXT NOT NULL,
+                information TEXT NOT NULL
+            )
+''')
+conn.commit()
+conn.close()
